fdf80badfe70748b52302b8fd3f633c0ee7c425e
panyi <yuyi.py@alibaba-inc.com>
2018-08-06 13:10:59 +0800

    add res for node server

--- hello.js
+++ hello.js
@@ -1,12 +1,35 @@
 var http = require('http');
+var mime = require('mime');
+var fs = require('fs');
+var path = require('path');
+
 var PORT = 3389;
+
+//console.log('xxx.png = '+ mime.getType('xxx.png'));
 
 http.createServer(
 	function(req , res){
 		console.log('req url = ' + req.url);
-		res.setHeader('Content-Type',"text/html;charset=utf-8");
-		res.end('SB!!! 呆逼方建');
+		if(req.url.startsWith('/img') || req.url.startsWith('/res')){
+			handleRes(req , res);
+		}else{
+			res.setHeader('Content-Type',"text/html;charset=utf-8");
+			res.end('404！！！！！！木哈哈哈哈');
+		}
 	}
 ).listen(PORT , function(){
 	console.log('server is running on ' + PORT + ' ...');
 });
+
+function handleRes(req , res){
+	fs.readFile(path.join(__dirname , req.url) , function(err , data){
+		if(err){
+			res.http = 404;
+			res.end('404 not found resource');
+			return;
+		}
+
+		res.setHeader('Content-Type' , mime.getType(req.url));
+		res.end(data);
+	});	
+}
